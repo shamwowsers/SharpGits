6c3a3a8cefb1a037d63c02887534d188457a242a
Joel Dineen <Joel@litmus.com>
2023-09-07 23:18:16 -0500

    Added DB implementation

--- SharpGits.Console/Data/Database.cs
+++ SharpGits.Console/Data/Database.cs
@@ -2,9 +2,25 @@ namespace SharpGits.Console.Data;
 
 public class Database
 {
+    private const string GitSubDirName = ".git";
+    private const string RefsSubDirName = "refs";
+    private const string ObjectsSubDirName = "objects";
+
     public static void Init(string workspacePath)
     {
-        // TODO Create git database directory and appropriate subdirs.
-        throw new NotImplementedException();
+        string gitPath = Path.Combine(workspacePath, GitSubDirName);
+        CreateGitDirectories(gitPath);
+        CreateHeadFile(gitPath);
     }
+
+    private static void CreateGitDirectories(string gitPath)
+    {        
+        Directory.CreateDirectory(gitPath);
+        Directory.CreateDirectory(Path.Combine(gitPath, RefsSubDirName));
+        Directory.CreateDirectory(Path.Combine(gitPath, ObjectsSubDirName));
+    }
+
+    private static void CreateHeadFile(string gitPath) =>
+        File.WriteAllText(path: Path.Combine(gitPath, "HEAD"), contents: @"ref: refs/heads/main" + Environment.NewLine);
+    
 }
